63844dba7cf0994af7cca3ec4377193c705dd72f
MSI\neoni <neonitize@gmail.com>
2021-11-27 17:01:12 +0000

    added a button and cube class

--- minecraftclone_ursina.py
+++ minecraftclone_ursina.py
@@ -5,8 +5,24 @@ class Test_cube(Entity):
         super().__init__(
             model = 'cube',
             color = color.white,
-            texture = 'white_cube'
-            )
+            texture = 'white_cube',
+            rotation = Vec3(45,45,45)
+        )
+    def input(self,key):
+        if self.hovered:
+            if key == 'left mouse down':
+                print('Button Pressed')
+
+class Test_button(Button):
+    def __init__(self):
+        super().__init__(
+            parent = scene,
+            model = 'cube',
+            texture = 'brick',
+            color = color.blue,
+            highlight_color = color.red,
+            pressed_color = color.green
+        )
 
 def update():
     if held_keys['a']:
@@ -17,9 +33,9 @@ app = Ursina()
 
 test_square = Entity(model = 'quad', color = color.red, scale = (1,4), position = (5,4))
 
-tamarin_texture = load_texture('assets/tamarin.jpg')
-tamarin = Entity(model = 'quad', texture = tamarin_texture, scale = (5,5))
+# tamarin_texture = load_texture('assets/tamarin.jpg')
+# tamarin = Entity(model = 'quad', texture = tamarin_texture, scale = (5,5))
 
-test_cube = Test_cube()
+test_cube = Test_button()
 
 app.run()
