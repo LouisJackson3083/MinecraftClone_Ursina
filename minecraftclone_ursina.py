8506a31f4a0ccb5056d3baefdcbe46f28acab07a
MSI\neoni <neonitize@gmail.com>
2021-11-27 19:10:23 +0000

    Added skybox

--- minecraftclone_ursina.py
+++ minecraftclone_ursina.py
@@ -6,6 +6,25 @@ grass_texture = load_texture('assets/grass_block.png')
 dirt_texture = load_texture('assets/dirt_block.png')
 stone_texture = load_texture('assets/stone_block.png')
 brick_texture = load_texture('assets/brick_block.png')
+sky_texture = load_texture('assets/skybox.png')
+arm_texture = load_texture('assets/arm_texture.png')
+punch_sound = Audio('assets/punch_sound', loop = False, autoplay = False)
+block_pick = 1
+
+window.fps_counter.enabled = False
+window.exit_button = False
+
+def update():
+    global block_pick
+    if held_keys['left mouse'] or held_keys['right mouse']:
+        arm.active()
+    else:
+        arm.passive()
+
+    if held_keys['1']: block_pick = 1
+    if held_keys['2']: block_pick = 2
+    if held_keys['3']: block_pick = 3
+    if held_keys['4']: block_pick = 4
 
 class Voxel(Button):
     def __init__(self, position = (0,0,0), texture = grass_texture):
@@ -21,15 +40,48 @@ class Voxel(Button):
     def input(self,key):
         if self.hovered:
             if key == 'left mouse down':
+                punch_sound.play()
                 destroy(self)
-            if key == 'right mouse down':
-                voxel = Voxel(position = self.position + mouse.normal, texture = stone_texture)
 
+            if key == 'right mouse down':
+                punch_sound.play()
+                if block_pick == 1: voxel = Voxel(position = self.position + mouse.normal, texture = grass_texture)
+                if block_pick == 2: voxel = Voxel(position = self.position + mouse.normal, texture = dirt_texture)
+                if block_pick == 3: voxel = Voxel(position = self.position + mouse.normal, texture = stone_texture)
+                if block_pick == 4: voxel = Voxel(position = self.position + mouse.normal, texture = brick_texture)
+
+class Sky(Entity):
+    def __init__(self):
+        super().__init__(
+            parent = scene,
+            model = 'sphere',
+            texture = sky_texture,
+            scale = 1000,
+            double_sided = True)
+
+class Arm(Entity):
+    def __init__(self):
+        super().__init__(
+            parent = camera.ui,
+            model = 'assets/arm',
+            texture = arm_texture,
+            scale = 0.2,
+            rotation = Vec3(160,-10,0),
+            position = Vec2(0.6,-0.6))
+    def active(self):
+        self.rotation =  Vec3(160,-20,0),
+        self.position = Vec2(0.5,-0.5)
+    def passive(self):
+        self.rotation =  Vec3(160,-10,0),
+        self.position = Vec2(0.6,-0.6)
 
 for z in range(20):
     for x in range(20):
         voxel = Voxel(position = (x,0,z))
 
 player = FirstPersonController()
+arm = Arm()
+sky = Sky()
+
 
 app.run()
