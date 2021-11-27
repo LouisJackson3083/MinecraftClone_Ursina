4e8e5d846cb1be361552d8db10d53258d9f8258f
MSI\neoni <neonitize@gmail.com>
2021-11-27 17:33:51 +0000

    added some basic textures

--- minecraftclone_ursina.py
+++ minecraftclone_ursina.py
@@ -1,24 +1,30 @@
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
+app = Ursina()
+
+grass_texture = load_texture('assets/grass_block.png')
+dirt_texture = load_texture('assets/dirt_block.png')
+stone_texture = load_texture('assets/stone_block.png')
+brick_texture = load_texture('assets/brick_block.png')
 
 class Voxel(Button):
-    def __init__(self, position = (0,0,0)):
+    def __init__(self, position = (0,0,0), texture = grass_texture):
         super().__init__(
             parent = scene,
             position = position,
-            model = 'cube',
+            model = 'assets/block',
             origin_y = 0.5,
-            texture = 'white_cube',
+            texture = texture,
             color = color.color(0,0,random.uniform(0.9,1)),
-            highlight_color = color.lime)
+            highlight_color = color.white,
+            scale = 0.5)
     def input(self,key):
         if self.hovered:
             if key == 'left mouse down':
                 destroy(self)
             if key == 'right mouse down':
-                voxel = Voxel(position = self.position + mouse.normal)
+                voxel = Voxel(position = self.position + mouse.normal, texture = stone_texture)
 
-app = Ursina()
 
 for z in range(20):
     for x in range(20):
