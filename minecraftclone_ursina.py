8f2418f09d62a9f1674d9ff24f5e4b3319651ca6
MSI\neoni <neonitize@gmail.com>
2021-11-29 12:45:44 +0000

    added trees. trying to figure out chunks

--- minecraftclone_ursina.py
+++ minecraftclone_ursina.py
@@ -7,6 +7,8 @@ texture_grass = [load_texture('assets/textures/block_grass.png'),load_texture('a
 texture_dirt = load_texture('assets/textures/block_dirt.png')
 texture_stone = load_texture('assets/textures/block_stone.png')
 texture_brick = load_texture('assets/textures/block_brick.png')
+texture_log = load_texture('assets/textures/block_log.png')
+texture_leaves = load_texture('assets/textures/block_leaves.png')
 texture_none = load_texture('assets/textures/block_invisible.png')
 texture_sky = load_texture('assets/textures/skybox.png')
 texture_arm = load_texture('assets/textures/texture_arm.png')
@@ -14,14 +16,28 @@ texture_arm = load_texture('assets/textures/texture_arm.png')
 # region sound
 sound_punch = Audio('assets/sounds/sound_punch', loop = False, autoplay = False)
 # endregion
+
 hotbar_selection = 1
+render_mode = 0
 
 window.fps_counter.enabled = False
 window.exit_button = False
 
+def switch_render_mode():
+    global render_mode
+    render_mode+=1
+    if render_mode>=4: render_mode=0
+    window.render_mode = window.render_modes[render_mode]
+
+def player_setup():
+    player.height = 1.6
+    player.camera_pivot.y = player.height
+    player.jump_height = 1.6
+    #player.collider = BoxCollider(player, center=Vec3(0,0.9,0), size=Vec3(0.8,1.9,0.8))
+
 def input(key):
     if key == 'c':
-        level_parent.collision = not level_parent.collision
+        switch_render_mode()
 
 def update():
     global hotbar_selection
@@ -32,16 +48,41 @@ def update():
 
     if held_keys['1']: 
         hotbar_selection = 1
-        arm_block.update_block(hotbar_selection)
+        arm_block.update_block(texture_grass[3])
     if held_keys['2']: 
         hotbar_selection = 2
-        arm_block.update_block(hotbar_selection)
+        arm_block.update_block(texture_dirt)
     if held_keys['3']: 
         hotbar_selection = 3
-        arm_block.update_block(hotbar_selection)
+        arm_block.update_block(texture_stone)
     if held_keys['4']: 
         hotbar_selection = 4
-        arm_block.update_block(hotbar_selection)
+        arm_block.update_block(texture_brick)
+    if held_keys['5']: 
+        hotbar_selection = 5
+        arm_block.update_block(texture_log)
+    if held_keys['6']: 
+        hotbar_selection = 6
+        arm_block.update_block(texture_leaves)
+
+def create_tree(position = (0,0,0), height = 9, size = 7):
+    tree = []
+    for i in range(size):
+        for j in range(height-4,height+1):
+            for k in range(size):
+                if (((i==0 or i==size-1) or (k==0 or k==size-1)) and (j==height-size+1 or j==height)) or ((i==0 or i==size-1) and (k==0 or k==size-1) or (j < height-1 and i == size//2 and k == size//2)):
+                    continue
+                else:
+                    voxel = Voxel(position=position+(i-size//2, j, k-size//2), texture = texture_leaves)
+    for i in range(height-1):
+        voxel = Voxel(position=position+(0, i, 0), texture = texture_log)
+
+def create_chunk(xChunk,zChunk):
+    for z in range(16):
+        for x in range(16):
+            #height = round(GeneratedNoiseMap(z, x, 20) * maxHeight)
+            voxel = Voxel(position=(x + xChunk*16, 0, z + zChunk*16), texture = random.choice(texture_grass))
+            voxel = Voxel(position=(x + xChunk*16, -1, z + zChunk*16), texture = texture_dirt)                
 
 class Voxel(Button):
     def __init__(self, position = (0,0,0), texture = texture_grass[3]):
@@ -62,10 +103,14 @@ class Voxel(Button):
 
             if key == 'right mouse down':
                 sound_punch.play()
+                #e = Entity(model='cube', parent=scene, origin_y = 0.25, collider = 'box', texture = texture_brick, position=self.position+mouse.normal)
+                
                 if hotbar_selection == 1: voxel = Voxel(position = self.position + mouse.normal, texture = random.choice(texture_grass))
                 if hotbar_selection == 2: voxel = Voxel(position = self.position + mouse.normal, texture = texture_dirt)
                 if hotbar_selection == 3: voxel = Voxel(position = self.position + mouse.normal, texture = texture_stone)
                 if hotbar_selection == 4: voxel = Voxel(position = self.position + mouse.normal, texture = texture_brick)
+                if hotbar_selection == 5: voxel = Voxel(position = self.position + mouse.normal, texture = texture_log)
+                if hotbar_selection == 6: voxel = Voxel(position = self.position + mouse.normal, texture = texture_leaves)
 
 class Sky(Entity):
     def __init__(self):
@@ -101,33 +146,27 @@ class ArmBlock(Entity):
             scale = 0.75,
             rotation = Vec3(180,0,0),
             position = Vec3(-0.5,-0.25,-3))
-    def update_block(self, hotbar_selection):
-        print(hotbar_selection)
-        if hotbar_selection == 1:
-            self.texture = texture_grass[3]
-        if hotbar_selection == 2:
-            self.texture = texture_dirt
-        if hotbar_selection == 3:
-            self.texture = texture_stone
-        if hotbar_selection == 4:
-            self.texture = texture_brick
+    def update_block(self, texture = texture_grass[3]):
+        self.texture = texture
 
 
-level_parent = Entity(model=Mesh(vertices=[], uvs=[]))
-hotbar = Entity(model='quad', scale=(5,1), position=(0,-1), parent = camera.ui)
+level_colliders = Entity(model=Mesh(vertices=[], uvs=[]))
+# hotbar = Entity(model='quad', scale=(5,1), position=(0,-1), parent = camera.ui)
 
-for z in range(8):
-    for x in range(8):
-        #height = round(GeneratedNoiseMap(z, x, 20) * maxHeight)
-        voxel = Voxel(position=(x, 0, z), texture = random.choice(texture_grass))
-        voxel = Voxel(position=(x, -1, z), texture = texture_dirt)
-        
+create_chunk(0,0)
+create_chunk(0,1)
+create_chunk(1,0)
+create_chunk(1,1)
+
+create_tree(position = (10,0,10))
 
 player = FirstPersonController()
 arm = Arm()
 arm_block = ArmBlock(arm, texture_grass[3])
 sky = Sky()
+player_setup()
 player_collision_zone = CollisionZone(parent=player, radius=2)
-level_parent.collider = 'mesh' # call this only once after all vertices are set up
+level_colliders.collider = 'mesh' # call this only once after all vertices are set up
 #print(level_parent.model.vertices)
+
 app.run()
